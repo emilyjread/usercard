11dbfca92e4997a3f3c84038393a87c8f53ddb53
Emily Read <emilyread01@gmail.com>
2021-06-26 12:05:27 -0700

    Added JS functionality

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function changeName(){
-    document.querySelector(#p)
+    document.querySelector(".profile-name").innerText = "Jeanne Uxbridge"
+}
+
+var connectionReqs = 2
+var yourConnections = 417
+function hide(element){ 
+    if(element.className=="accept icon"){
+        console.log("accepted!")
+        yourConnections--
+        document.querySelector("#connections-number").innerText = yourConnections
+    }
+    element.parentNode.parentNode.parentNode.removeChild(element.parentNode.parentNode);
+    connectionReqs--;
+    document.querySelector("#connection-requests").innerText = connectionReqs;
+   
 }
